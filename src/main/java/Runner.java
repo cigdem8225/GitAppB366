860f3c0dd8617d5872930dc30421556a354f1bd6
cigdemeraslan <amancigdem25@gmail.com>
2025-09-25 22:26:36 +0300

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -5,5 +5,10 @@ public class Runner {
         System.out.println("Hello GIT");
         System.out.println("git add . : Staging ares ya alır.");
 
+
+        //Version 2
+        System.out.println("git commit -m \\\"message\\\" ile versiyon oluşturulur.");
+
+
     }
 }
