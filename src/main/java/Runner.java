42780d227a851d97e4bc941584342dd1e2bea325
cigdemeraslan <amancigdem25@gmail.com>
2025-09-25 22:18:29 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //Version 1
+        System.out.println("Hello GIT");
+        System.out.println("git add . : Staging ares ya alır.");
+
+    }
+}
